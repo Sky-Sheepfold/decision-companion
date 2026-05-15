0c3340ebec1982a1440d22fb2446f4aa59eb3d25
Sheepfold <2786735826@qq.com>
2026-05-15 16:01:52 +0800

    feat: 初始化后端项目骨架

---
+++ src/main/java/com/sky/decisioncompanion/DecisionCompanionApplication.java
@@ -0,0 +1,13 @@
+package com.sky.decisioncompanion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecisionCompanionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DecisionCompanionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sky/decisioncompanion/DecisionCompanionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sky.decisioncompanion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecisionCompanionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
